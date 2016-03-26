06bf6b61d4e22f07249c887d538acbfd79795e6e
Santiago Suarez Perez <suarez.santiago.perez@gmail.com>
2016-03-25 22:45:41 -0500

    still fixing errors 4 /music

--- client/app/media/music/music.controller.js
+++ client/app/media/music/music.controller.js
@@ -1,42 +1,28 @@
 'use strict';
 
-var lastfmapi = require('lastfmapi');
-
-/*var lfm = new lastfmapi {
-	'api_key': process.env.LASTFM_ID,
-	'api_secret': process.env.LASTFM_SECRET
-}*/
-
-var lfm = new lastfmapi({
-	'api_key': 'daf892b5e71b6eb11332fd993874f2b5',
-	'api_secret': '19b0ee51f9ac7a5a71b73e03ec0f2f1d'
-});
-
-var params = {
-	'country': 'Colombia',
-	'limit': 10
-}
-
-function handleData(err, data){
-	if(err){
-		console.log("### PANIC ###");
-		return console.log(err);
-	}
-	/*console.log("Successfully fetched top tracks in Colombia");
-	console.log(data);
-	console.log(data.track[0].image[1]["#text"]);
-	console.log(data.track[0].artist);*/
-	$scope.music = data.track;
-}
-//un-comment in DEV
-//lfm.geo.getTopTracks(params, handleData);
-
-
 module.exports = function(app){
 	app.controller('MusicCtrl', function($scope, $http, socket){
+		var lastfmapi = require('lastfmapi');
+		/*var lfm = new lastfmapi {
+			'api_key': process.env.LASTFM_ID,
+			'api_secret': process.env.LASTFM_SECRET
+		}*/
+		var lfm = new lastfmapi({
+			'api_key': 'daf892b5e71b6eb11332fd993874f2b5',
+			'api_secret': '19b0ee51f9ac7a5a71b73e03ec0f2f1d'
+		});
 		$scope.music = [];
 		$scope.waiting = true;
-		lfm.geo.getTopTracks(params, handleData);
+		lfm.geo.getTopTracks({
+			'country': 'Colombia',
+			'limit': 10
+		}, function(err, data){
+			if(err){
+				console.log("### PANIC ###");
+				return console.log(err);
+			}
+			$scope.music = data.track;
+		});
 		$scope.waiting = false;
 	});
 }
