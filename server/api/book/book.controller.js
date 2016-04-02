ad2c5f604e6561a9975f52000103cc5276d94baa
Alan Kelvin <alankelvin1992@gmail.com>
2016-04-02 16:16:43 -0500

    Adding search to books api

--- server/api/book/book.controller.js
+++ server/api/book/book.controller.js
@@ -43,9 +43,23 @@ function getSubject (subject){
       );
     }
   );
-
 }
 
+function searchForTitle (query){
+  return new Promise(
+    (resolve, reject) => {
+      request.get(`http://openlibrary.org/search.json?q=`+query,
+        (err, res, body) => {
+          if(err){
+            reject(err);
+          }
+          var books = JSON.parse(body).docs;
+          resolve(books);
+        }
+      );
+    }
+  );
+}
 
 exports.index = function(req, res) {
   Promise.map(
@@ -59,4 +73,17 @@ exports.index = function(req, res) {
     log.error("On API /books, ERROR: %s",error);
     res.send(error);
   });
+};
+
+exports.search = function(req, res) {
+  var query = req.params.query;
+  searchForTitle(query)
+  .then((books)=>{
+    res.json(books);
+  }).catch((error) => {
+    log.error("On API /books, ERROR: %s",error);
+    res.send(error);
+  });
+
+
 };
